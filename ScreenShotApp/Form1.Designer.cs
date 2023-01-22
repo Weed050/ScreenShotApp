fa206952e10a4ed1d9ba164897a197525bd05410
Weed050 <84383140+Weed050@users.noreply.github.com>
2023-01-22 17:11:10 +0100

    basic changes

--- ScreenShotApp/Form1.Designer.cs
+++ ScreenShotApp/Form1.Designer.cs
@@ -28,13 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.chooseFolderBTN = new System.Windows.Forms.Button();
             this.textBoxDisplay = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // chooseFolderBTN
             // 
-            this.chooseFolderBTN.Location = new System.Drawing.Point(249, 199);
+            this.chooseFolderBTN.Location = new System.Drawing.Point(360, 192);
             this.chooseFolderBTN.Name = "chooseFolderBTN";
             this.chooseFolderBTN.Size = new System.Drawing.Size(131, 29);
             this.chooseFolderBTN.TabIndex = 0;
@@ -44,20 +47,34 @@
             // 
             // textBoxDisplay
             // 
-            this.textBoxDisplay.Location = new System.Drawing.Point(41, 192);
+            this.textBoxDisplay.Location = new System.Drawing.Point(21, 192);
             this.textBoxDisplay.Name = "textBoxDisplay";
-            this.textBoxDisplay.Size = new System.Drawing.Size(125, 27);
+            this.textBoxDisplay.Size = new System.Drawing.Size(160, 27);
             this.textBoxDisplay.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(479, 174);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(503, 231);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.textBoxDisplay);
             this.Controls.Add(this.chooseFolderBTN);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Screen Shot App";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -67,5 +84,6 @@
 
         private Button chooseFolderBTN;
         private TextBox textBoxDisplay;
+        private PictureBox pictureBox1;
     }
 }
