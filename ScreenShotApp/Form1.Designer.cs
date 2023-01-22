07979eee6ba9662426ba82dfb446fc40b23bea07
Weed050 <84383140+Weed050@users.noreply.github.com>
2023-01-22 17:11:00 +0100

    can select a folder from directory and basic changes

--- ScreenShotApp/Form1.Designer.cs
+++ ScreenShotApp/Form1.Designer.cs
@@ -28,19 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.chooseFolderBTN = new System.Windows.Forms.Button();
+            this.textBoxDisplay = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // chooseFolderBTN
+            // 
+            this.chooseFolderBTN.Location = new System.Drawing.Point(249, 199);
+            this.chooseFolderBTN.Name = "chooseFolderBTN";
+            this.chooseFolderBTN.Size = new System.Drawing.Size(131, 29);
+            this.chooseFolderBTN.TabIndex = 0;
+            this.chooseFolderBTN.Text = "Choose a folder";
+            this.chooseFolderBTN.UseVisualStyleBackColor = true;
+            this.chooseFolderBTN.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxDisplay
+            // 
+            this.textBoxDisplay.Location = new System.Drawing.Point(41, 192);
+            this.textBoxDisplay.Name = "textBoxDisplay";
+            this.textBoxDisplay.Size = new System.Drawing.Size(125, 27);
+            this.textBoxDisplay.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(503, 231);
+            this.Controls.Add(this.textBoxDisplay);
+            this.Controls.Add(this.chooseFolderBTN);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Button chooseFolderBTN;
+        private TextBox textBoxDisplay;
     }
 }
--- ScreenShotApp/Form1.cs
+++ ScreenShotApp/Form1.cs
@@ -6,5 +6,18 @@ namespace ScreenShotApp
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog diag = new FolderBrowserDialog();
+            if (diag.ShowDialog() == DialogResult.OK)
+            {
+                textBoxDisplay.Text = diag.SelectedPath;
+            }
+            else
+            {
+                textBoxDisplay.Text = "You didn't choose a folder";
+            }
+        }
     }
 }
